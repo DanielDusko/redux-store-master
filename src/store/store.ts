40d6d8efb3e237aecffab48f5c6ed5d56c231aff
DanielDusko <denim@poczta.fm>
2019-01-27 18:09:45 +0100

    Last

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,10 +4,37 @@ export class Store {
   private state: { [key: string]: any };
 
   constructor(reducers = {}, initialState = {}) {
-    this.state = initialState;
+    this.subscribes = [];
+    this.reducers = reducers;
+    this.state = this.reduce(initialState, {});
   }
 
   get value() {
     return this.state;
+  }
+
+  subscribe(fn) {
+    this.subscribes = [...this.subscribes, fn];
+    this.notify();
+    return () => {
+      this.subscribes = this.subscribes.filter(sub => sub !== fn);
+    };
+  }
+
+  dispatch(action) {
+    this.state = this.reduce(this.state, action);
+    this.notify();
+  }
+
+  private notify() {
+    this.subscribes.forEach(fn => fn(this.value));
+  }
+
+  private reduce(state, action) {
+    const newState = {};
+    for (const prop in this.reducers) {
+      newState[prop] = this.reducers[prop](state[prop], action);
+    }
+    return newState;
   }
 }
